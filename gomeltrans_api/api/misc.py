a3c6c9dfd6aa995c16d1c7e7a00c72706f1a2ec1
dop3file <49581209+dop3file@users.noreply.github.com>
2022-09-15 22:36:18 +0300

    hot fix and rename

--- gomeltrans_api/api/misc.py
+++ gomeltrans_api/api/misc.py
@@ -1,13 +1,24 @@
+from datetime import datetime
+
 from .utils import load_data_from_json
 
 
-def _get_route_with_stops(stop_from: str, stop_to: str, is_weekday: bool) -> dict:
+def is_weekday():
+    return datetime.now().weekday() < 5
+
+
+def sort_nearest_routes(routes: list[dict]):
+    #TODO
+    pass
+
+
+def _get_route_from_stops(stop_from: str, stop_to: str) -> dict:
     all_transports_info: dict[dict] = {
         'bus': load_data_from_json('bus'), 
         'trolleybus': load_data_from_json('trolleybus')
     }
 
-    suitable_routes = []
+    suitable_routes: list[dict] = []
 
     for transport_type, transport in all_transports_info.items():
         add_route = lambda side: suitable_routes.append({
@@ -17,9 +28,8 @@ def _get_route_with_stops(stop_from: str, stop_to: str, is_weekday: bool) -> dic
                 })
         for number, info in transport.items():
             get_stop_index = lambda global_side, side_index: list(info['stops'][global_side].keys()).index(side_index)
-
             if all([info['stops']['to'].get(stop) for stop in (stop_from, stop_to)]) and get_stop_index('to', stop_to) > get_stop_index('to', stop_from):
                 add_route('to')
             elif all([info['stops']['back'].get(stop) for stop in (stop_from, stop_to)]) and get_stop_index('back', stop_to) > get_stop_index('back', stop_from):
                 add_route('back')
-    print(suitable_routes)
+    
--- gomeltrans_api/gomeltrans_api/urls.py
+++ gomeltrans_api/gomeltrans_api/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
 from django.urls import path
-from api.views import get_route, get_all_name_routes, get_route_with_stops
+from api.views import get_route, get_all_name_routes, get_route_from_stops
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('route/<str:type_transport>/name_all/', get_all_name_routes, name='get_all_routes_name'),
     path('route/<str:type_transport>/<str:number>/', get_route, name='get_route'),
-    path('route/route_with_stops/', get_route_with_stops, name='get_route_with_stops')
+    path('route/route_from_stops/', get_route_from_stops, name='get_route_with_stops')
 ]
