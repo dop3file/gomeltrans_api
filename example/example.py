de54ff3613dea92ad4052fe5af21addba1fb5194
dop3file <49581209+dop3file@users.noreply.github.com>
2022-09-19 18:46:58 +0300

    hot fix

--- example/example.py
+++ example/example.py
@@ -2,10 +2,8 @@ from datetime import datetime
 import requests
 
 
-#from_stop = input('Введите остановку, с которой вам надо уехать: ')
-#to_stop = input('Введите остановку, на которую вам надо прихеать: ')
-from_stop = 'Улица Оськина'
-to_stop = 'Переулок Ильича'
+from_stop = input('Введите остановку, с которой вам надо уехать: ')
+to_stop = input('Введите остановку, на которую вам надо прихеать: ')
 
 avialable_transports = requests.get(f"https://gomeltrans.pythonanywhere.com/route/routes_from_stops/?from={from_stop}&to={to_stop}").json()['response']
 nearest_transport = requests.get(f"https://gomeltrans.pythonanywhere.com/route/nearest_route/?from={from_stop}&to={to_stop}").json()['response']
@@ -14,12 +12,14 @@ for transport in avialable_transports:
     type_transport = 'Автобус' if transport['transport_type'] == 'bus' else 'Тролейбус'
     print(f'{transport["number"]} {type_transport}')
 
-print('-' * 10)
-print('Самый ближайший транспорт: ')
-type_transport = 'Автобус' if nearest_transport["route_info"]["transport_type"] == 'bus' else 'Тролейбус'
-transport_stop_time = datetime.strptime(nearest_transport["stop_time"], '%H:%M')
-print(transport_stop_time)
-now = datetime.now()
-time_delta: list = str(transport_stop_time - now).split(" ")[-1].split(":")[0:2]
+try:
+    type_transport = 'Автобус' if nearest_transport["route_info"]["transport_type"] == 'bus' else 'Тролейбус'
+    transport_stop_time = datetime.strptime(nearest_transport["stop_time"], '%H:%M')
+    print('-' * 10)
+    print('Самый ближайший транспорт: ')
+    now = datetime.now()
+    time_delta: list = str(transport_stop_time - now).split(" ")[-1].split(":")[0:2]
 
-print(f'{nearest_transport["route_info"]["number"]} {type_transport} - осталось {time_delta[0] + " часа " if time_delta[0] != "0" else ""}{time_delta[1]} минуты')  
+    print(f'{nearest_transport["route_info"]["number"]} {type_transport} - осталось {time_delta[0] + " часа " if time_delta[0] != "0" else ""}{time_delta[1]} минуты')
+except TypeError:
+    print('Такого маршрута нет!')
--- gomeltrans_api/api/misc.py
+++ gomeltrans_api/api/misc.py
@@ -4,11 +4,11 @@ from time import time
 from .utils import load_data_from_json
 
 
-TIME_NOW = datetime.now() + timedelta(hours=9)
+TIME_NOW = lambda: datetime.now()
 
 
 def is_weekday():
-    return TIME_NOW.weekday() < 5
+    return TIME_NOW().weekday() < 5
 
 def _sort_nearest_routes(routes: list[dict], stop_from: str):
     '''
@@ -20,16 +20,17 @@ def _sort_nearest_routes(routes: list[dict], stop_from: str):
         stop_times = []
 
         for index, route in enumerate(routes):
-            stop_times.extend([[index, datetime(year=TIME_NOW.year, month=TIME_NOW.month, day=TIME_NOW.day, hour=int(stop_time.split(':')[0]), minute=int(stop_time.split(':')[1]))] for stop_time in load_data_from_json(route['transport_type'])[route['number']]['stops'][route['side']][stop_from]['week' if is_weekday() else 'weekend']])
+            stop_times.extend([[index, datetime(year=TIME_NOW().year, month=TIME_NOW().month, day=TIME_NOW().day, hour=int(stop_time.split(':')[0]), minute=int(stop_time.split(':')[1]))] for stop_time in load_data_from_json(route['transport_type'])[route['number']]['stops'][route['side']][stop_from]['week' if is_weekday() else 'weekend']])
 
-        nearest_route = [stop_time for stop_time in stop_times if (stop_time[1] - TIME_NOW).total_seconds() > 0]
-        print(TIME_NOW)
+        nearest_route = [stop_time for stop_time in stop_times if (stop_time[1] - TIME_NOW()).total_seconds() > 0]
+        print(TIME_NOW())
         return {
                 "route_info": routes[nearest_route[0][0]],
-                "stop_time": nearest_route[0][1].strftime("%H:%M")
+                "stop_time": nearest_route[0][1].strftime("%H:%M"),
+                "local_time": TIME_NOW()
                 }
 
-        
+
     except IndexError:
         return None
 
@@ -41,8 +42,8 @@ def _get_route_from_stops(stop_from: str, stop_to: str) -> dict:
     stop_to - остановка, на которую надо прибыть
     '''
     all_transports_info: dict[dict] = {
-        'bus': load_data_from_json('bus'), 
-        'trolleybus': load_data_from_json('trolleybus')
+        'trolleybus': load_data_from_json('trolleybus'),
+        'bus': load_data_from_json('bus')
     }
 
     suitable_routes: list[dict] = []
@@ -59,5 +60,5 @@ def _get_route_from_stops(stop_from: str, stop_to: str) -> dict:
                 add_route('to')
             elif all([info['stops']['back'].get(stop) for stop in (stop_from, stop_to)]) and get_stop_index('back', stop_to) > get_stop_index('back', stop_from):
                 add_route('back')
-    
+
     return suitable_routes
